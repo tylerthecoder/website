ae39f250b45ea31faa5a600934f7d20cee69a059
Tyler Tracy <tylertracy1999@gmail.com>
2021-12-02 22:34:30 -0600

    Added books

---
+++ pages/books.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from "next";
+
+
+const Books: NextPage = () => {
+
+
+	return (
+		<div>
+			<p> On the origin of conscience and the breakdown of the bicameral mind </p>
+		</div>
+	)
+}
+
+export default Books;
